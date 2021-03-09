65d5aa07fa3c15a48ae19f195f1755cce8021a99
Jonatas Oliveira <jonatas.gna@gmail.com>
2021-03-09 16:03:50 -0300

    created enum client type abd test to it

---
+++ src/test/kotlin/Teste/TesteClienteEnum.kt
@@ -0,0 +1,19 @@
+package Teste
+
+import one.digitalinovation.digibankone.ClienteTipo
+
+fun main() {
+    ClienteTipo.values().forEach { elemento ->
+    println("${elemento.name} - ${elemento.descricao}")
+    }
+
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
+}
