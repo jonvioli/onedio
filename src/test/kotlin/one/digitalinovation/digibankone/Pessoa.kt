dfdb12c3f9d26f7ef158ea14b4844b283ed18609
Jonatas Oliveira <jonatas.gna@gmail.com>
2021-03-08 21:18:52 -0300

    Pessoa classe

---
+++ src/test/kotlin/one/digitalinovation/digibankone/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinovation.digibankone
+
+class Pessoa {
+    var nome: String = "Jonatas"
+    var cpf: String = "123.123.123-12"
+}
+
+fun main() {
+    val Jonatas = Pessoa()
+        println(Jonatas.nome)
+        println(Jonatas.cpf)
+    }
