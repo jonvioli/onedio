47eff55e8cd5dd5a17d7d2830c9c22db86e2f700
Jonatas Oliveira <jonatas.gna@gmail.com>
2021-03-09 15:21:33 -0300

    Created Banco class and Test to It

---
+++ src/test/kotlin/Teste/Bancoteste.kt
@@ -0,0 +1,15 @@
+package Teste
+
+import one.digitalinovation.digibankone.Banco
+
+fun main() {
+    val digiOneBank = Banco(nome = "DigiOne", numero = 12)
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    val banco2 = digiOneBank.copy(nome = "Banco2")
+
+    println(banco2.info())
+
+}
