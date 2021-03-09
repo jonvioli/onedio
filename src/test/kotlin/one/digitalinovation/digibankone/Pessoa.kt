edb36b79927cb654cc40833cfbb1c69cac2cfd90
Jonatas Oliveira <jonatas.gna@gmail.com>
2021-03-08 21:42:18 -0300

    Private set

--- src/test/kotlin/one/digitalinovation/digibankone/Pessoa.kt
+++ src/test/kotlin/one/digitalinovation/digibankone/Pessoa.kt
@@ -3,10 +3,12 @@ package one.digitalinovation.digibankone
 class Pessoa {
     var nome: String = "Jonatas"
     var cpf: String = "123.123.123-12"
+    private set
 }
 
 fun main() {
     val Jonatas = Pessoa()
+    println(Jonatas)
         println(Jonatas.nome)
         println(Jonatas.cpf)
     }
